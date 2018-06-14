5c7c30b60ddbd73cb28280bb5e8a8b4ab3a5c8d7
Olaf Ennen <olaf.ennen@coremedia.com>
2018-06-14 16:15:22 +0200

    added new type for MediaContextValue

--- src/types.js
+++ src/types.js
@@ -1,11 +1,4 @@
 // @flow
-export type FragmentParams = {
-  color?: string,
-  ctaShow?: boolean,
-  ctaText?: string,
-  url?: string,
-};
-
 export type Config = {
   queryName: string,
   viewName: ?string,
@@ -40,4 +33,30 @@ export type Breakpoints = {
 
 export type Theme = {
   [key: string]: mixed,
+};
+
+export type FragmentProps = {
+  fragmentType: string,
+  viewType: string,
+  params?: Object,
+  data: Object,
+};
+
+export type FragmentParams = {
+  color?: string,
+  ctaShow?: boolean,
+  ctaText?: string,
+  url?: string,
+};
+
+export type FragmentState = {
+  fragmentType?: string,
+  viewType?: string,
+  config?: Config,
+  Component?: Function,
+  componentProps?: Object,
+};
+
+export type MediaContextValue = {
+  getMediaUrl: (link: string, ratio?: string, minWidth?: number) => string,
 };
